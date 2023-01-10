279148bd9168e24cfc97c467e6ec2430f0a0fdb1
cyberriver <vblagirev@gmail.com>
2023-01-10 20:30:48 +0300

    fixed migrations

--- db/migrate/20221120074108_add_references_question_to_test.rb
+++
@@ -1,5 +0,0 @@
-class AddReferencesQuestionToTest < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :questions, :test, foreign_key: true 
-  end
-end
--- db/migrate/20221203165502_drop_tests.rb
+++
@@ -1,5 +0,0 @@
-class DropTests < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :tests
-  end
-end
