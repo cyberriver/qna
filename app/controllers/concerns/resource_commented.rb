f96a8b9e87d7ad395c2d60e39f02aa0fe7cc7867
cyberriver <vblagirev@gmail.com>
2023-03-06 00:27:46 +0300

    fixed rendering comments for answers and questions

--- app/controllers/concerns/resource_commented.rb
+++ app/controllers/concerns/resource_commented.rb
@@ -12,15 +12,25 @@ module ResourceCommented
 
   def find_resource_commented
     @klass = params[:comment][:commentable_type].capitalize.constantize
+    @klass_route = params[:comment][:commentable_type]
     @resource_commented = @klass.find(params[:comment][:commentable_id])    
   end
 
   def choose_route
-    case params[:comment][:commentable_type]
-      when 'question' then 'questions_path'
-      when 'answer' then 'question_answers_path'
-      else 'questions_path'
+    puts "LOGGG params[:comment][:commentable_type] #{params[:comment][:commentable_type]}"
+    case @klass_route 
+      when "Question"
+        'questions_path'
+      when "Answer"
+        "question_path(#{get_params_for_route})"
+      else 
+        'root_path'
     end   
   end
+
+  def get_params_for_route
+    @resource_commented.question.id if @klass_route == "Answer"
+  end
+
 
 end
