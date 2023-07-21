96ae204a0ae0aa91cd9f43b8c162437567fb07cb
cyberriver <vblagirev@gmail.com>
2023-07-21 08:18:44 +0300

    revised ajax queries

--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -1,30 +1,33 @@
 $(document).on('turbolinks:load', function() {
+  // Устанавливаем CSRF-Token для всех ajax-запросов
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+
   $('.answers').on('click', '.edit-answer-link', function(e) {
     e.preventDefault();
     $(this).hide();
     var answerId = $(this).data('answerId');
     $('form#edit-answer-' + answerId).removeClass('hidden');
+  });
 
-  })
-  
   $('form.new-answer').on('ajax:success', function(e) {
-      var answer = e.detail[0];
-
+    var answer = e.detail[0];
     $('.answers').append('<p>' + answer.title + '</p>');
-  })
-    .on('ajax:error', function(e){
-      var errors = e.detail[0];
-      $.each(errors, function(index,value){
-        $(".answer-errors").append('<p> rating:' + value + '</p>')
-      })
-    })
+  }).on('ajax:error', function(e){
+    var errors = e.detail[0];
+    $.each(errors, function(index,value){
+      $(".answer-errors").append('<p> rating:' + value + '</p>');
+    });
+  });
 
-    $('.answers').on('ajax:success', function(e) {
-      var data = e.detail[0];
-      var rating = data.rating
-      var resourceId = data.resource_id
-
-      $('#rating_answer_' + resourceId).text('rating'+ rating);      
-    })
+  $('.answers').on('ajax:success', function(e) {
+    var data = e.detail[0];
+    var rating = data.rating;
+    var resourceId = data.resource_id;
+    $('#rating_answer_' + resourceId).text('rating'+ rating);      
+  });
 });
 
