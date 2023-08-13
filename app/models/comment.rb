a7eee8dbba05cdda65b399648c2619041de6b194
cyberriver <vblagirev@gmail.com>
2023-08-13 17:28:20 +0300

    added cashing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
   belongs_to :author, class_name: 'User'
 
   validates :title,  presence: true
